650efb88192bc498eed504efb5d6fe654bae3d55
kimpenguin <kimmypeng@gmail.com>
2016-02-19 14:15:47 -0500

    add readme

--- MODIS-IRI/LandSurfaceTemp/get_data_IRI_MODIS1km.sh
+++ MODIS-IRI/LandSurfaceTemp/get_data_IRI_MODIS1km.sh
@@ -3,9 +3,10 @@
 #written by Kimberly Peng
 # This scripts downloads the time series average for the Land surface temperature and creates an Africa mosaic reprojected to Lambert Azimuthal Equal Area.
 
-#Example:
+#Examples:
 #/data4/afsisdata/IRI_MODIS/scripts/./get_data_IRI_MODIS1km.sh LSTD Jul 2002 Jun 2015 /data7/MODIS/LSTD
 #/data4/afsisdata/IRI_MODIS/scripts/./get_data_IRI_MODIS1km.sh LSTN Jul 2002 Jun 2015 /data7/MODIS/LSTN
+
 
 #parameters
 Dataset=$1
@@ -53,4 +54,8 @@ time gdal_merge.py -n NaN -of GTiff -o $OutputDir/"$Dataset"_avgIRI_"$Month1""$Y
 
 # reproject from Geographic to Lambert Azimuthal Equal Area
 time gdalwarp -overwrite -s_srs '+proj=longlat +datum=WGS84 +no_defs' -t_srs '+proj=laea +lat_0=5 +lon_0=20 +x_0=0 +y_0=0 +ellps=WGS84 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs' $OutputDir/"$Dataset"_avgIRI_"$Month1""$Year1"_"$Month2""$Year2"'_mosaic.tif' $OutputDir/"$Dataset"_avgIRI_"$Month1""$Year1"_"$Month2""$Year2"'_mosaicLAEA.tif' -multi -wm 5000
+
+# #changes the permissions for the calculated average and sum
+# chmod 775 $OutputDir/"$Dataset"_avgIRI_"$Month1""$Year1"_"$Month2""$Year2"'_mosaicLAEA.tif'
+
 
---
+++ MODIS-IRI/readme.md
@@ -0,0 +1,17 @@
+## This directory contains the scripts used to develop the MODIS Vegetation Indices and Land Surface Temperature data products for the Africa Soil Information Service (AfSIS). The raw data and initial processing was done using the IRI Data Library http://iridl.ldeo.columbia.edu/.
+
+
+The data products include the following:
+
+**VegetationIndices** - 250m resolution
+
+* Enhanced Vegetation Index (EVI)
+* Normalized Difference Vegetation Index (NDVI)
+* Reflectance Red Band 1
+* Reflectance Near-Infrared (NIR) Band 2
+* Reflectance Blue Band 3
+* Reflectance Mid-Infrared (MIR) Band 7
+
+**LandSurfaceTemp** - 1km resolution
+* Land Surface Temperature Day
+* Land Surface Temperature Night
